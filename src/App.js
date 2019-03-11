2b97c80e5b3f30fb93b3784947e7620b9661795c
hugo arvis <arvishugo@hotmail.frgit config --global user.email arvishugo@hotmail.fr>
2019-03-11 19:55:22 +0100

    fourth commit use a router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import { Container, Row } from 'react-bootstrap';
 import ProfilePage from './components/ProfilePage/ProfilePage';
-
-
-
+import { Route, BrowserRouter, NavLink } from 'react-router-dom';
+import UpComingEventsTab from'./components/UpComingEventsTab/UpComingEventsTab';
+import  FriendsTab from './components/FriendsTab/FriendsTab';
+import './App.css';
 class App extends Component {
   render() {
-    return (<div>
+    return (<div className="allElements">
 
      <ProfilePage />
-     
+     <BrowserRouter>
+      <Container>
+
+        <Row className='bottomLine'>
+          <ul className='tabBar'>
+            <li className='tabBar'>
+              <NavLink to="/players/:id/upcoming" exact activeStyle={
+                {
+                  color: 'black',
+                  fontWeight: 'bold',
+                  fontSize: '1.5vw',
+                  borderBottomStyle: 'solid',
+                  borderBottomColor: 'grey',
+                  paddingBottom: '0.4vw',
+                }
+
+              }>Upcomings event</NavLink></li>
+            <li className='tabBar'>
+              <NavLink to="/players/:id/friends" exact activeStyle={
+                {
+                  color: 'black',
+                  fontWeight: 'bold',
+                  fontSize: '1.5vw',
+                  borderBottomStyle: 'solid',
+                  borderBottomColor: 'grey',
+                  paddingBottom: '0.4vw',
+
+
+                }
+              }>Friends</NavLink>
+            </li>
+          </ul>
+
+        </Row>
+        <Route path="/" exact strict component={UpComingEventsTab} />
+        <Route path="/players/:id" exact strict component={UpComingEventsTab} />
+        <Route path="/players/:id/upcoming" exact strict component={UpComingEventsTab} />
+        <Route path="/players/:id/friends" exact strict component={FriendsTab} />
+
+      </Container>
+    </BrowserRouter>
      
       </div>
     );
--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -3,7 +3,7 @@ import { Container, Row } from 'react-bootstrap';
 import './ProfilePage.css';
 
 import UserPanel from '../UserPanel/UserPanel';
-import TabBar from '../TabBar/TabBar';
+
 
 
 export default class ProfilePage extends Component {
@@ -13,7 +13,7 @@ export default class ProfilePage extends Component {
         <Row className="rowSize">
 
           <Col-4><UserPanel/></Col-4>
-          <Col-7><TabBar /></Col-7>
+          <Col-7></Col-7>
 
         </Row>
       </Container>
